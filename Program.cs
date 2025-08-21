6e9c50d08a72038986631e70edbe7a45a63e7fe8
Diego <diegosdo0911@gmail.com>
2025-08-20 23:04:43 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia meuNokia = new Nokia("1234-5678", "Nokia 3310", "IMEI123456");
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("WhatsApp");
+
+Iphone meuIphone = new Iphone("9876-5432", "iPhone 13", "IMEI654321");
+meuIphone.Ligar();
+meuIphone.InstalarAplicativo("Telegram");
